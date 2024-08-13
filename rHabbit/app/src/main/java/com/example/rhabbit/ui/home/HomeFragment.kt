4036ad8d96c1d489d775d8f8b4aa0698503c6fd9
Pyotr-Ivanov <p.schulmeyer@campus.tu-berlin.de>
2024-08-14 00:26:56 +0200

    corrected home interace

--- rHabbit/app/src/main/java/com/example/rhabbit/ui/home/HomeFragment.kt
+++ rHabbit/app/src/main/java/com/example/rhabbit/ui/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +14,21 @@ import com.example.rhabbit.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+
+    // layout
+    private lateinit var btRs : Button  // reset Button
+
+    // habit 1
+    private lateinit var pbHab1 : ProgressBar
+    private lateinit var lbPHab1: TextView  // lbProgressHabit1
+    private lateinit var lbGHab1: TextView  // lbGoalHabit1
+    private lateinit var btAHab1: Button    // btAddHabit1
+
+    // habit 2
+    private lateinit var pbHab2 : ProgressBar
+    private lateinit var lbPHab2: TextView  // lbProgressHabit1
+    private lateinit var lbGHab2: TextView  // lbGoalHabit1
+    private lateinit var btAHab2: Button    // btAddHabit1
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,10 +45,16 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        /*
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+         */
+
+
+
+
         return root
     }
 
--- rHabbit/app/src/main/java/com/example/rhabbit/ui/notifications/NotificationsFragment.kt
+++ rHabbit/app/src/main/java/com/example/rhabbit/ui/notifications/NotificationsFragment.kt
@@ -28,10 +28,12 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        /*
         val textView: TextView = binding.textNotifications
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+         */
         return root
     }
 
